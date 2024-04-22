1597c11e6b64fa0373fcddf51ac7ee72f7625e48
marc.catrisse <marc.catrisse@upc.edu>
2024-04-23 00:14:15 +0200

    fix bugs

--- bucket.tf
+++ bucket.tf
@@ -21,7 +21,7 @@ resource "aws_kms_key" "this" {
 resource "aws_kms_alias" "this" {
   count = length(aws_kms_key.this) #Conditional creation
   name          = "alias/${var.kms_key_alias}"
-  target_key_id = aws_kms_key.this.key_id
+  target_key_id = aws_kms_key.this[0].key_id
 }
 
 #---------------------------------------------------------------------------------------------------
@@ -104,7 +104,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = var.s3_bucket_sse_algorithm
-      kms_master_key_id = var.s3_bucket_sse_algorithm == "aws:kms" ? aws_kms_key.this.arn : null
+      kms_master_key_id = var.s3_bucket_sse_algorithm == "aws:kms" ? aws_kms_key.this[0].arn : null
     }
   }
 }
